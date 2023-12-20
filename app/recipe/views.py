45951bcec7c552dfdf2ade19b9496aac7d8c3bdc
Danish <danishjamal525@gmail.com>
2023-12-21 01:01:22 +0530

    image api created

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,7 +1,7 @@
 """
 views for recipe apis
 """
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from core.models import (
@@ -9,6 +9,8 @@ from core.models import (
     Recipe,
     Tag
 )
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from recipe import serializers
 
 
@@ -27,11 +29,25 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """return the serialzer for request"""
         if self.action == 'list':
             return serializers.RecipeSerializer
+        elif self.action == 'upload_image':
+            return serializers.RecipeImageSerializer
         return self.serializer_class
 
     def perform_create(self, serializer):
         """create a new recipe"""
         serializer.save(user=self.request.user)
+
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """upload an image to recipe"""
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class BaseRecipeAttrViewSet(mixins.UpdateModelMixin,
